29405d99d4a5d1926415836a2a566db947aaa3b4
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-04-02 18:15:56 +0300

    Add files via upload

---
+++ OdisseyGame/client/src/main/java/client/main/CLIApplication.java
@@ -0,0 +1,27 @@
+package client.main;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import library.domain.ClientSidePlayer;
+import library.domain.Player;
+
+public class CLIApplication {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// 1 create a player
+		Player player = new ClientSidePlayer("Player1", 0);
+		
+		
+		System.out.println("CLIENT: Player created" + player);
+		// 2 open a network socket
+		Socket clientSocket = new Socket("localhost", 10000);
+		System.out.println("CLIENT: connected to server");
+		// 3 send player object
+		OutputStream outputStream = clientSocket.getOutputStream(); // factory pattern
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+		objectOutputStream.writeObject(player);
+	}
+}
