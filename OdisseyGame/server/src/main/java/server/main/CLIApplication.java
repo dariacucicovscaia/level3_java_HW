66ce7987efc176831de62698c2125d10ae961c5c
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-04-18 09:00:33 +0300

    Add files via upload

--- OdisseyGame/server/src/main/java/server/main/CLIApplication.java
+++ OdisseyGame/server/src/main/java/server/main/CLIApplication.java
@@ -2,31 +2,66 @@ package server.main;
 
 import java.io.IOException;
 
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import library.domain.Player;
+import library.tom.Game;
+import library.tom.Gun;
+import library.tom.Missile;
+import library.tom.Ship;
+import library.tom.Space;
 
 public class CLIApplication {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		//1 listen/run server
-		ServerSocket serverSocket = new ServerSocket(10000);
-		System.err.println("SERVER: Server started ... ");
+//		ServerSocket serverSocket = new ServerSocket(10000);
+//		System.err.println("SERVER: Server started ... ");
+//		
+//		//2 accept client connection
+//		Socket clientSocket = serverSocket.accept();
+//		System.err.println("SERVER: Player connected ... ");
+//		
+//		//3 receive player 
+//		InputStream inputStream = clientSocket.getInputStream();
+//		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+//		
+//		//4 print player in console
+//		Player player =  Player.class.cast( objectInputStream.readObject());
+//		
+//		System.err.println("SERVER: received " + player);
 		
-		//2 accept client connection
-		Socket clientSocket = serverSocket.accept();
-		System.err.println("SERVER: Player connected ... ");
+		Game game = new Game();
+		game.appendChild(new Space());
+		game.getFirstChild().appendChild(new Ship());
+		game.getFirstChild().appendChild(new Ship());
+
+		game.getFirstChild().getFirstChild().appendChild(new Gun());
+		game.getFirstChild().getLastChild().appendChild(new Gun());
 		
-		//3 receive player 
-		InputStream inputStream = clientSocket.getInputStream();
-		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 		
-		//4 print player in console
-		Player player =  Player.class.cast( objectInputStream.readObject());
-		
-		System.err.println("SERVER: received " + player);
+		// the Ship shoots a missile from it's gun
+		game.getFirstChild().getFirstChild().getFirstChild().appendChild(new Missile());
+        game.getFirstChild().getFirstChild().getFirstChild().removeChild(0);
+        game.appendChild(new Missile());
+        game.getChildren();
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
