c04ca379902ec10c1413651b113b2e1d72a7cc9b
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-04-26 11:42:00 +0300

    Add files via upload
    server-side xml implementation

--- OdisseyGame/server/src/main/java/server/main/CLIApplication.java
+++ OdisseyGame/server/src/main/java/server/main/CLIApplication.java
@@ -1,67 +1,34 @@
 package server.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import library.tom.Game;
-import library.tom.Gun;
-import library.tom.Missile;
-import library.tom.Ship;
-import library.tom.Space;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import XML.XMLLoader;
+import XML.XMLTraverser;
 
 public class CLIApplication {
-	
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		//1 listen/run server
-//		ServerSocket serverSocket = new ServerSocket(10000);
-//		System.err.println("SERVER: Server started ... ");
-//		
-//		//2 accept client connection
-//		Socket clientSocket = serverSocket.accept();
-//		System.err.println("SERVER: Player connected ... ");
-//		
-//		//3 receive player 
-//		InputStream inputStream = clientSocket.getInputStream();
-//		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-//		
-//		//4 print player in console
-//		Player player =  Player.class.cast( objectInputStream.readObject());
-//		
-//		System.err.println("SERVER: received " + player);
-		
-		Game game = new Game();
-		game.appendChild(new Space());
-		game.getFirstChild().appendChild(new Ship());
-		game.getFirstChild().appendChild(new Ship());
 
-		game.getFirstChild().getFirstChild().appendChild(new Gun());
-		game.getFirstChild().getLastChild().appendChild(new Gun());
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException {
+
+		Element root = new XMLLoader().load("src/main/resources/tom.xml");
+		System.out.println(root.getNodeName());
+		// System.out.println(new XMLTraverser().getIntoNode(root.));
+		XMLTraverser xmlt = new XMLTraverser(root);
+		List<Node> children = xmlt.getChildren();
+//				xmlt.getChildren()  // default root
+//				.get(0));           // space
 		
-		
-		// the Ship shoots a missile from it's gun
-		game.getFirstChild().getFirstChild().getFirstChild().appendChild(new Missile());
-        game.getFirstChild().getFirstChild().getFirstChild().removeChild(0);
-        game.appendChild(new Missile());
-        game.getChildren();
+
+		children.stream().forEach(System.out::println);
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
